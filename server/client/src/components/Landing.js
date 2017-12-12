c0126e2a04d9dcec6e0e37fbfbb7b4c037a44dc3
ThirdGhostHand <ThirdGhostHand@outlook.com>
2017-12-12 11:52:39 -0800

    intial commit

---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        SurveySender!
+      </h1>
+      Send surveys to people! (We make it easier!)
+    </div>
+  );
+};
+
+export default Landing;
